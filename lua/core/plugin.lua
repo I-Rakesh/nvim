8c5d8ea98231f12054d751a8c1bbadbf3dc0f3a3
I.Rakesh <indurthirakesh2@gmail.com>
2023-09-30 19:19:41 +0530

    Fixed some problems with :checkhealth

--- lua/core/plugin.lua
+++ lua/core/plugin.lua
@@ -148,9 +148,8 @@ local plugins = {
     -- Markdown preview
     {
       "iamcco/markdown-preview.nvim",
-      run = function() vim.fn["mkdp#util#install"]() end,
+      build = function() vim.fn["mkdp#util#install"]() end,
     },
-    { "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, }
 }
 local opts = {}
 
