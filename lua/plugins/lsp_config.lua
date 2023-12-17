5930b59006b8392bc0b87d4993e80dc08b77b5b8
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-17 16:41:00 +0530

    Added some more remaps for nvim-lspconfig

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -76,8 +76,19 @@ return {
 		require("lspconfig").bashls.setup({})
 		require("lspconfig").marksman.setup({})
 
-		vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "lsp Goto previous diagnostic" })
-		vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "lsp Goto next diagnostic" })
+		local diagnostic_goto = function(next, severity)
+			local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+			severity = severity and vim.diagnostic.severity[severity] or nil
+			return function()
+				go({ severity = severity })
+			end
+		end
+		vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+		vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+		vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+		vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+		vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+		vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
