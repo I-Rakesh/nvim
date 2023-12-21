32abf8954238d17127102a415765c2d558f32a23
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-21 07:52:12 +0530

    Changed theme to catppuccin

--- lua/plugins/colorscheam.lua
+++ lua/plugins/colorscheam.lua
@@ -1,31 +1,38 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	dependencies = {
-		"navarasu/onedark.nvim",
-	},
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd([[colorscheme  gruvbox ]])
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      vim.o.background = "dark" -- or "light" for light mode
+      vim.cmd([[colorscheme  catppuccin ]])
 
-		require("gruvbox").setup({
-			transparent_mode = true,
-		})
-		require("onedark").setup({
-			transparent = true,
-			lualine = {
-				transparent = true,
-			},
-		})
+      require("onedark").setup({
+        transparent = true,
+        lualine = {
+          transparent = true,
+        },
+      })
 
-		function ColorMyPencils(color)
-			color = color or "gruvbox"
-			vim.cmd.colorscheme(color)
-
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		end
-
-		ColorMyPencils()
-	end,
+      -- Function to make any theme transprant
+      -- function ColorMyPencils(color)
+      --   color = color or "catppuccin"
+      --   vim.cmd.colorscheme(color)
+      --
+      --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      --   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      -- end
+      --
+      -- ColorMyPencils()
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe",        -- latte, frappe, macchiato, mocha
+        transparent_background = true, -- disables setting the background color.
+      })
+    end,
+  },
 }
