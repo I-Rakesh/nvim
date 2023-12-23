1e2e46c008e3357b03d8360099da5fa9760ec38c
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-23 16:40:04 +0530

    Added remaps for oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -12,6 +12,10 @@ return {
         ["~"] = "actions.tcd",
         ["H"] = "actions.toggle_hidden",
         ["<CR>"] = "actions.select",
+        ["<C-x>"] = "actions.select_vsplit",
+        ["<C-h>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
       },
     })
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
