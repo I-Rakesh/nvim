3116cbadd0b2ed0ef7a817639a7f7127147a654a
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-23 18:46:02 +0530

    Added lazy load on keymaps for comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,14 @@
 return {
-	"numToStr/Comment.nvim",
-	opts = {
-		-- add any options here
-	},
-	event = "VeryLazy",
+  "numToStr/Comment.nvim",
+  keys = {
+    { "gcc", mode = "n",          desc = "Comment toggle current line" },
+    { "gc",  mode = { "n", "o" }, desc = "Comment toggle linewise" },
+    { "gc",  mode = "x",          desc = "Comment toggle linewise (visual)" },
+    { "gbc", mode = "n",          desc = "Comment toggle current block" },
+    { "gb",  mode = { "n", "o" }, desc = "Comment toggle blockwise" },
+    { "gb",  mode = "x",          desc = "Comment toggle blockwise (visual)" },
+  },
+  opts = {
+    -- add any options here
+  },
 }
