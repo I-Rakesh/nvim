66713462d9d640d349d94c00b759efd6ef02e6cf
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-28 21:38:07 +0530

    Testing dap for cpp

---
+++ lua/plugins/dap_config.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function() end,
+  },
+}
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -45,6 +45,8 @@ return {
         "shfmt",
         "shellcheck",
         "mypy",
+        --Debugger
+        "codelldb",
       },
     })
 
