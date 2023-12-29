410d4f26dcb736f593a146a707618a5509ca0d22
I.Rakesh <indurthirakesh2@gmail.com>
2023-12-29 07:31:35 +0530

    Debugger has be set up (beta)

--- lua/plugins/dap_config.lua
+++ lua/plugins/dap_config.lua
@@ -1,36 +1,185 @@
 return {
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-dap",
+  keys = {
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Breakpoint Condition",
     },
-    opts = {
-      handlers = {},
+    {
+      "<leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Toggle Breakpoint",
+    },
+    {
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Continue",
+    },
+    {
+      "<leader>dC",
+      function()
+        require("dap").run_to_cursor()
+      end,
+      desc = "Run to Cursor",
+    },
+    {
+      "<leader>dg",
+      function()
+        require("dap").goto_()
+      end,
+      desc = "Go to line (no execute)",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<leader>dj",
+      function()
+        require("dap").down()
+      end,
+      desc = "Down",
+    },
+    {
+      "<leader>dk",
+      function()
+        require("dap").up()
+      end,
+      desc = "Up",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Run Last",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<leader>dp",
+      function()
+        require("dap").pause()
+      end,
+      desc = "Pause",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Toggle REPL",
+    },
+    {
+      "<leader>ds",
+      function()
+        require("dap").session()
+      end,
+      desc = "Session",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Terminate",
+    },
+    {
+      "<leader>dw",
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      desc = "Widgets",
     },
   },
-  {
-    "mfussenegger/nvim-dap",
-    config = function() end,
+  dependencies = {
+    {
+      "rcarriga/nvim-dap-ui",
+      dependencies = "mfussenegger/nvim-dap",
+      config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup()
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open()
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          dapui.close()
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          dapui.close()
+        end
+      end,
+    },
+    {
+      "jay-babu/mason-nvim-dap.nvim",
+      dependencies = {
+        "williamboman/mason.nvim",
+        "mfussenegger/nvim-dap",
+      },
+      config = function()
+        require("mason-nvim-dap").setup({
+          handlers = {
+            function(config)
+              -- all sources with no handler get passed here
+
+              -- Keep original functionality
+              require("mason-nvim-dap").default_setup(config)
+            end,
+            codelldb = function(config)
+              local dap = require("dap")
+              dap.configurations.cpp = {
+                {
+                  name = "Launch file",
+                  type = "codelldb",
+                  request = "launch",
+                  program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                  end,
+                  cwd = "${workspaceFolder}",
+                  stopOnEntry = false,
+                },
+              }
+              dap.adapters.codelldb = {
+                type = "server",
+                port = "${port}",
+                executable = {
+                  -- CHANGE THIS to your path!
+                  command = "/Users/rakesh/.local/share/nvim/mason/packages/codelldb/extension/adapter/codelldb",
+                  args = { "--port", "${port}" },
+
+                  -- On windows you may have to uncomment this:
+                  -- detached = false,
+                },
+              }
+              require("mason-nvim-dap").default_setup(config)
+            end,
+          },
+        })
+      end,
+    },
   },
+  config = function() end,
 }
