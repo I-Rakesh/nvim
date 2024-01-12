1f303e715e461cc831e7089b89679160824b510a
I.Rakesh <indurthirakesh2@gmail.com>
2024-01-12 15:59:46 +0530

    Changed cmp-cmdline.lua config into its own file

--- lua/plugins/cmp-cmdline.lua
+++ lua/plugins/cmp-cmdline.lua
@@ -4,5 +4,28 @@ return {
 		"/",
 		":",
 	},
-	config = function() end,
+	config = function()
+		local cmp = require("cmp")
+		-- `/` cmdline setup.
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man" },
+					},
+				},
+			}),
+		})
+	end,
 }
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -93,26 +93,5 @@ return {
 				documentation = cmp.config.window.bordered(),
 			},
 		})
-		-- `/` cmdline setup.
-		cmp.setup.cmdline("/", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
-		-- `:` cmdline setup.
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{
-					name = "cmdline",
-					option = {
-						ignore_cmds = { "Man" },
-					},
-				},
-			}),
-		})
 	end,
 }
