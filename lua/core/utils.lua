600c8887aabc710bd8b3decdd2a5ac14afc67b63
I.Rakesh <indurthirakesh2@gmail.com>
2024-01-15 16:14:34 +0530

    Added function to return cursor to the same place as before

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,5 +1,17 @@
 local M = {}
 
+-- go to last loc when opening a buffer
+-- this mean that when you open a file, you will be at the last position
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
