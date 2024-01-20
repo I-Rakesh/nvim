e481dbdef17c5a5365ed3289fbcc1484ead887b5
I.Rakesh <indurthirakesh2@gmail.com>
2024-01-20 07:58:56 +0530

    Fixed remap for diffview.plugin

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -2,7 +2,7 @@ return {
   "sindrets/diffview.nvim",
   keys = {
     { "<leader>dv",  mode = { "n" }, "<cmd>DiffviewOpen<CR>",          desc = "DiffView" },
-    { "<leader>dcv", mode = { "n" }, "<cmd>DiffviewOpen<CR>",          desc = "DiffView Specific File Commit" },
+    { "<leader>dcv", mode = { "n" }, ":DiffviewOpen ",                 desc = "DiffView Specific File Commit" },
     { "<leader>dh",  mode = { "n" }, "<cmd>DiffviewFileHistory %<CR>", desc = "DiffView Current File Histoyr" },
     { "<leader>dbh", mode = { "n" }, "<cmd>DiffviewFileHistory<CR>",   desc = "DiffView Current Branch Histoyr" },
     { "<leader>dn",  mode = { "n" }, "<cmd>DiffviewToggleFiles<CR>",   desc = "DiffView File Tree" },
