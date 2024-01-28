d57892f49a20644ecfc6c727c182c06aad3f43c9
I.Rakesh <indurthirakesh2@gmail.com>
2024-01-28 23:42:16 +0530

    Small changes

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -77,7 +77,7 @@ return {
       },
     })
 
-    require("lspconfig").lua_ls.setup({
+    lspconfig.lua_ls.setup({
       settings = {
         Lua = {
           diagnostics = {
@@ -93,7 +93,7 @@ return {
       },
     })
 
-    require("lspconfig").clangd.setup({
+    lspconfig.clangd.setup({
       cmd = {
         "clangd",
         "--offset-encoding=utf-16",
