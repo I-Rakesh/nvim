2eeea7a1df56fcc7bf634c7b6b875dfe7c15ff26
I.Rakesh <indurthirakesh2@gmail.com>
2024-02-01 11:47:08 +0530

    Added vim-dadbod and vim-dadbod-ui,completions for database management

---
+++ lua/plugins/database.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "tpope/vim-dadbod",
+    config = function() end,
+  },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    keys = {
+      { "<leader>du", mode = "n", "<cmd>DBUIToggle<CR>", desc = "Detabase UI Toggle" },
+    },
+    dependencies = {
+      { "tpope/vim-dadbod",                     lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+}
