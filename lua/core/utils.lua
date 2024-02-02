f5dc4fde4fac113b143f9d7ddc1ad54dc104be6d
I.Rakesh <indurthirakesh2@gmail.com>
2024-02-02 22:21:10 +0530

    Added autocmd to keep cursor highlight on active window

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,5 +1,17 @@
 local M = {}
 
+-- show cursor line only in active window
+local cursorGrp = vim.api.nvim_create_augroup("CursorLine", { clear = true })
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+vim.api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
 -- Terminal
 vim.cmd([[autocmd TermOpen * setlocal nonumber norelativenumber nospell signcolumn=no ]])
 -- vim.cmd([[autocmd TermOpen * startinsert]])
