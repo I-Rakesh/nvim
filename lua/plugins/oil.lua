bb290104fe50c7376c48c03c0333646e2c44d695
I.Rakesh <indurthirakesh2@gmail.com>
2024-02-08 04:57:26 +0530

    Updated oil.nvim settings

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,21 +2,10 @@ return {
   "stevearc/oil.nvim",
   config = function()
     require("oil").setup({
-      use_default_keymaps = false,
+      -- use_default_keymaps = false,
 
       keymaps = {
-        ["g?"] = "actions.show_help",
-        ["-"] = "actions.parent",
-        ["_"] = "actions.open_cwd",
-        ["`"] = "actions.cd",
-        ["~"] = "actions.tcd",
         ["H"] = "actions.toggle_hidden",
-        ["<CR>"] = "actions.select",
-        ["gv"] = "actions.select_vsplit",
-        ["gx"] = "actions.select_split",
-        ["gs"] = "actions.change_sort",
-        ["gt"] = "actions.select_tab",
-        ["<C-p>"] = "actions.preview",
         ["<A-r>"] = "actions.refresh",
       },
     })
