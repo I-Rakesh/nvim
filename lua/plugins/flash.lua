d20d5d1ce3b0dbd5567c2fa42e40c095baaa2bb8
I.Rakesh <indurthirakesh2@gmail.com>
2024-02-19 15:43:24 +0530

    Trying to live without Flash Treesitter

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -22,9 +22,9 @@ return {
     { "F",     mode = { "n", "v" } },
     { "t",     mode = { "n", "v" } },
     { "T",     mode = { "n", "v" } },
-    { "s",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    -- { "s",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = "o",          function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R",     mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" },      function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
   },
 }
