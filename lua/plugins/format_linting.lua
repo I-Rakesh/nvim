a8e451097ef5b24195983bb965a1eb08bb555173
I.Rakesh <indurthirakesh2@gmail.com>
2024-02-24 16:09:51 +0530

    Fixed the nvim-lint issue

--- lua/plugins/format_linting.lua
+++ lua/plugins/format_linting.lua
@@ -33,30 +33,23 @@ return {
     "mfussenegger/nvim-lint",
     lazy = true,
     config = function()
-      return {
-        "mfussenegger/nvim-lint",
-        lazy = true,
-        event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-        config = function()
-          require("lint").linters_by_ft = {
-            python = { "mypy", "ruff" },
-            sh = { "Shellcheck" },
-          }
-
-          local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-          vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-            group = lint_augroup,
-            callback = function()
-              require("lint").try_lint()
-            end,
-          })
-
-          vim.keymap.set("n", "<leader>l", function()
-            require("lint").try_lint()
-          end, { desc = "Trigger linting for current file" })
-        end,
+      require("lint").linters_by_ft = {
+        python = { "mypy", "ruff" },
+        sh = { "Shellcheck" },
       }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+        group = lint_augroup,
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        require("lint").try_lint()
+      end, { desc = "Trigger linting for current file" })
     end,
   },
 }
