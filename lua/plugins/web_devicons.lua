d06e6adddf4052663654c2dafaef19f96fe83072
I.Rakesh <indurthirakesh2@gmail.com>
2024-03-03 22:20:06 +0530

    Changed icon for go and java

---
+++ lua/plugins/web_devicons.lua
@@ -0,0 +1,17 @@
+return {
+  "kyazdani42/nvim-web-devicons",
+  opts = {
+    override_by_extension = {
+      ["go"] = {
+        icon = "",
+        color = "#00ADD8",
+        name = "go",
+      },
+      ["java"] = {
+        icon = "󰬷",
+        color = "#DB8AA2",
+        name = "java",
+      },
+    },
+  },
+}
