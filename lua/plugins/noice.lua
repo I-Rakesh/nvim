5e21be6bab0a91c41de898ad2b511334fecba279
I.Rakesh <indurthirakesh2@gmail.com>
2024-04-05 15:25:42 +0530

    Modified remap for scrolling Hover Doc using noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -77,7 +77,7 @@ return {
     { "<leader>nh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
     { "<leader>na", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
     { "<leader>nd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
-    { "<c-f>",      function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward",  mode = { "i", "n", "s" } },
-    { "<c-b>",      function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward", mode = { "i", "n", "s" } },
+    { "<c-d>",      function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll forward in Hover Doc",  mode = { "i", "n", "s" } },
+    { "<c-u>",      function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll backward in Hover Doc", mode = { "i", "n", "s" } },
   },
 }
