e553c1f703f793cb1b5677242d4b3684698bb1e1
I.Rakesh <indurthirakesh2@gmail.com>
2024-04-23 00:55:40 +0530

    Changed the search icon for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -151,7 +151,7 @@ return {
           "--column",
           "--smart-case",
         },
-        prompt_prefix = "   ",
+        prompt_prefix = "  ",
         selection_caret = "󰁕 ",
         entry_prefix = "  ",
         initial_mode = "insert",
