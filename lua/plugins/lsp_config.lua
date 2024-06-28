d1abb40ecf1ca3a75a8c0f5e35c96432e639ec2b
I.Rakesh <indurthirakesh2@gmail.com>
2024-06-28 23:50:06 +0530

    Small changes

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -103,6 +103,8 @@ return {
           go({ severity = severity })
         end
       end
+      vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+      vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
       vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
       vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
       vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
