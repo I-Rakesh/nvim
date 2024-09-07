abe39c18f6da15c77e2dc5f8fd04496c7ae40366
I.Rakesh <indurthirakesh2@gmail.com>
2024-09-07 12:36:57 +0530

    Added a plugin for previewing .md files

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,11 +1,19 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  ft = "markdown",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
-  config = function()
-    vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Open Markdown Preview" })
-    vim.g.mkdp_auto_close = 0
-  end,
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    config = function()
+      vim.keymap.set("n", "<C-p>", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Open Markdown Preview" })
+      vim.g.mkdp_auto_close = 0
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = "markdown",
+    opts = {},
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  },
 }
