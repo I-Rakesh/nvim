f1e56a67f812595064697f3c9c9bef7d4597612f
I.Rakesh <indurthirakesh2@gmail.com>
2025-03-18 00:16:04 +0530

    Added vim-rhubarb for opening in Git

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,10 +1,14 @@
 return {
   "tpope/vim-fugitive",
+  dependencies = {
+    "tpope/vim-rhubarb",
+  },
   keys = {
     { "<leader>gs", mode = "n", vim.cmd.Git, desc = "Git Status" },
     { "<leader>gD", mode = "n", "<cmd>Git difftool<CR>", desc = "Git Difftool" },
     { "<leader>gp", mode = "n", "<cmd>Git push<CR>", desc = "Git Push" },
     { "<leader>gc", mode = "n", "<cmd>Git commit<CR>", desc = "Git Commit" },
     { "<leader>gl", mode = "n", "<cmd>Git log<CR>", desc = "Git Log" },
+    { "<leader>gb", mode = "n", "<cmd>GBrowse<CR>", desc = "Open Github" },
   },
 }
